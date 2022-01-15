203fd1bda6a9b7d07a3a772cf76c0d6fae04c02d
Ky3bK4 <k.kyzmin321@gmail.com>
2022-01-15 02:29:43 +0200

    fix links

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,2 +1,2 @@
-export const IMAGE_CAP_URL_SMALL = "http://placehold.it/500x160"
-export const IMAGE_CAP_URL_BIG = "http://placehold.it/1180x600"
+export const IMAGE_CAP_URL_SMALL = "https://via.placeholder.com/500x160"
+export const IMAGE_CAP_URL_BIG = "https://via.placeholder.com/1180x600"
